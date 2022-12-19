bf771c2dfb8a85ffd87b5d92f832dac619f4181c
Willian Rafael <willian.rafael18@gmail.com>
2022-12-19 19:54:19 -0300

    Exercicio 01

---
+++ exercicio.js/exercicios/ex01.js
@@ -0,0 +1,13 @@
+/* 
+Crie uma função que retorna a string "Olá," 
+concatenada com um argumento text ( a ser passado para a função) 
+e com ponto de exclamação "!" no final.
+Exemplo: 
+    cumpimentar("Leonardo") -> retornará "Olá. Leonardo!"
+*/
+
+function cumpimentar(nome){
+    return nome
+}
+const nome = 'Leonardo'
+console.log('Olá, ' + nome + "!")
