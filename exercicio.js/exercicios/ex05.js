8482833c841d67fe65a8447183488db61cd568a9
Willian Rafael <willian.rafael18@gmail.com>
2022-12-22 15:03:09 -0300

    Exercicio 05

---
+++ exercicio.js/exercicios/ex05.js
@@ -0,0 +1,20 @@
+/*
+Crie uma função que receba dois números e retorne se o 
+primeiro é maior ou igual ao segundo.
+*/ 
+
+function maiorOuIgual(a, b){
+    if (a >= b){
+        console.log(true)
+    }else{
+        console.log(false)
+    }   
+    
+}
+
+
+maiorOuIgual(0,0)
+maiorOuIgual(5, 10)
+maiorOuIgual(0, "0")
+maiorOuIgual(-5, 10)
+maiorOuIgual(5, 1)
