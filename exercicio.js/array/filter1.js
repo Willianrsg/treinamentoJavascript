2a84d7fb0c8d814a591dd36e450dc678ff88bee3
Willian Rafael <willian.rafael18@gmail.com>
2023-01-21 17:29:38 -0300

    Array, MAP, FILTER

---
+++ exercicio.js/array/filter1.js
@@ -0,0 +1,15 @@
+const produto = [
+    {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'iPad', preco: 4199, fragil: true},
+    {nome: 'Copo de Vidro', preco: 12.49, fragil: true},
+    {nome: 'Copo de Plastico', preco: 18.99, fragil: false},
+]
+
+console.log(produto.filter(function(p){
+    return p.fragil === true && p.preco > 500
+}))
+ 
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produto.filter(caro).filter(fragil))
---
+++ exercicio.js/node/moduloCliente.js
@@ -0,0 +1,11 @@
+const modulaA = require('./moduloA')
+const modulaB = require('./moduloB')
+
+console.log(modulaA.ola)
+console.log(modulaA.bemVindo)
+console.log(modulaA.ateLogo)
+console.log(modulaA)
+
+console.log(modulaA.bomDia)
+console.log(modulaA.boaNoite())
+console.log(modulaA)
